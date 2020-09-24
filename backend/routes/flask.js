a6a8cd7a9aea3f91018a3146e0205f5748784118
DemonicMushy <damiencheng@icloud.com>
2020-09-24 15:46:51 +0800

    Removed dependency on requests module. Update other dependencies.

--- backend/routes/flask.js
+++ backend/routes/flask.js
@@ -1,52 +1,43 @@
 const express = require('express')
 const router = express.Router()
-const request = require('request')
+const axios = require('axios')
 const dotenv = require('dotenv');
 dotenv.config();
 
 /*Deep Neural Network Python API*/
 router.post("/api/russ_query", (req, res) => {
-    
+
     let query = req.body.question
 
-    request({
-        method:'POST',
-        url: `${process.env.FLASK_ENDPOINT}/api/query`,
-        json: {"request":query}
-    }, (error, response, body) =>{ 
-
-        if(error !== null){
-            if (error.errno === "ECONNREFUSED"){
-                res.json({ reply: "DNN server is down", queries:[]})
-            }
-        }else{
-            res.json({ reply: response.body.response, queries: response.body.reccomendation})
-        }
-    });
+    axios.post(`${process.env.FLASK_ENDPOINT}/api/query`, {
+      request: query,
+    })
+    .then( response => {
+      res.json({ reply: response.data.response, queries: response.data.reccomendation})
+    })
+    .catch( error => {
+      res.json({ reply: "DNN server is down" })
+    })
 
 });
 
 /*Response Comparison*/
 router.post("/api/responseCompare", (req,res)=>{
-    
+
     let query = req.body.responses
     // console.log(query)
 
-    request({
-        method:'POST',
-        url: `${process.env.FLASK_ENDPOINT}/api/similarityCheck`,
-        json: {"request":query}
-    }, (error, response, body) =>{
-        if (error !== null){
-            if(error.errno === "ECONNREFUSED"){
-                res.json({ reply:-1})
-            }
-        }else{
-            console.log(query)
-            console.log(response.body.response)
-            res.status(200).json({ reply: response.body.response})
-        }
-    });
+    axios.post(`${process.env.FLASK_ENDPOINT}/api/similarityCheck`, {
+      request: query,
+    })
+    .then( response => {
+      console.log(query)
+      console.log(response.data.response)
+      res.status(200).json({ reply: response.data.response })
+    })
+    .catch( error => {
+      res.json({ reply:-1 })
+    })
 
 });
 
